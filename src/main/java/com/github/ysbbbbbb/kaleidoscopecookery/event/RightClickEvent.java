723397844d287477cc0a2a0be2692a59b37449f0
MMRyanSheep <xuruiyang114@gmail.com>
2025-10-04 11:02:53 +0800

    现在车万女仆:魂符可以shift+右键放置在果篮上

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/event/RightClickEvent.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/event/RightClickEvent.java
@@ -7,6 +7,7 @@ import com.github.ysbbbbbb.kaleidoscopecookery.init.ModItems;
 import com.github.ysbbbbbb.kaleidoscopecookery.init.ModTrigger;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
@@ -16,9 +17,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = KaleidoscopeCookery.MOD_ID)
 public class RightClickEvent {
@@ -29,10 +32,11 @@ public class RightClickEvent {
         Player player = event.getEntity();
         InteractionHand hand = event.getHand();
         ItemStack itemInHand = player.getItemInHand(hand);
-
+        Item maidItem = ForgeRegistries.ITEMS.getValue(new ResourceLocation("touhou_little_maid", "smart_slab_has_maid"));
         if (player.isSecondaryUseActive() && hand == InteractionHand.MAIN_HAND
-            // FIXME: 目前仅排除调试棒，这导致其他方块无法潜行右击果篮使用
+            // FIXME: 目前仅排除调试棒 & 车万女仆:魂符，这导致其他方块无法潜行右击果篮使用
             && !itemInHand.is(Items.DEBUG_STICK)
+            && (maidItem == null || !itemInHand.is(maidItem))
             && level.getBlockEntity(pos) instanceof FruitBasketBlockEntity fruitBasketBlock) {
             fruitBasketBlock.takeOut(player);
             event.setCanceled(true);
