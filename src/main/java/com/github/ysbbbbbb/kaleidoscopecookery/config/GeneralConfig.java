b005f1e5b0483941755f19f6f15931620b517de1
tartaric_acid <baka943@qq.com>
2025-11-09 16:20:45 +0800

    调整饱腹代偿机制 - 新增饱腹代偿配置文件，可以选择关闭伤害溢出吸收功能。可在单次攻击超出玩家当前饱食度时，让额外的部分给玩家造成正常的伤害！
    fixed #73

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/config/GeneralConfig.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/config/GeneralConfig.java
@@ -5,8 +5,18 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class GeneralConfig {
     public static ForgeConfigSpec init() {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        builder.push("cookery");
-        builder.pop();
+        general(builder);
         return builder.build();
+    }
+
+    public static ForgeConfigSpec.BooleanValue SATIATED_SHIELD_ABSORB_EXCESS_DAMAGE;
+
+    private static void general(ForgeConfigSpec.Builder builder) {
+        builder.push("cookery");
+
+        builder.comment("Whether the Satiated Shield effect should absorb excess damage beyond its capacity.");
+        SATIATED_SHIELD_ABSORB_EXCESS_DAMAGE = builder.define("SatiatedShieldAbsorbExcessDamage", true);
+
+        builder.pop();
     }
 }
